4c41194fc89e967bb457c87c316e5230dcf41714
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2020-07-29 21:19:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+var msg = 'Hello Word!!';
+console.log(msg);
+msg.charAt;
---
+++ appPimentoes.js
@@ -0,0 +1,6 @@
+var string = 'Pimentões';
+var A = 11;
+var b = 7
+var total = A+b;
+console.log('Pimentões = ',total);
+total.toLocaleString
